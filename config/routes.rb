43113720be77db7adf7c0e2f813715de193e70b8
ogola5 <ogolaevance5@gmail.com>
2022-11-29 06:42:11 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  post "/signup", to: "sessions#create"
+  get "/me", to: "sessions#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
